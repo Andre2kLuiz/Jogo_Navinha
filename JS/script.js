5c6783ebf96e0e045321705776d5a108fd9d255f
André Luiz <123383093+Andre2kLuiz@users.noreply.github.com>
2023-08-08 17:23:15 -0300

    up

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,27 @@
 
 
-const buttonUp = document.querySelector('button')
-const input = document.querySelector("input");
+// Obtém uma referência para o botão no HTML
+const meuBotao = document.querySelector('meuBotao');
 
-buttonUp.addEventListener('touchmove', HandleStart, false)
+// Adiciona um ouvinte de evento para o clique no botão
+meuBotao.addEventListener('click', simularTeclaW);
+console.log(meuBotao)
+// Função para simular pressionar a tecla "W"
+function simularTeclaW() {
+  // Cria um evento do teclado simulando uma tecla pressionada
+  const eventoTeclaPressionada = new KeyboardEvent('keydown', {
+    key: 'w',
+    keyCode: 87,
+    which: 87,
+    code: 'KeyW',
+  });
+
+  // Despacha o evento para o documento
+  document.dispatchEvent(eventoTeclaPressionada);
+}
+
+
+
+
+
+
